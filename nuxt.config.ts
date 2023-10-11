0c9c0ae69735281cd3597c29ee46c7ba444c7190
Dan Vega <danvega@gmail.com>
2023-10-11 15:24:02 -0400

    Adding production build fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,5 +17,16 @@ export default defineNuxtConfig({
     injectPosition: 0,
     viewer: true
   },
+  // production build issue: https://answers.netlify.com/t/javascript-heap-out-of-memory-when-trying-to-build-a-nuxt-app/93138/13
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+          process.env.NODE_ENV === 'production'
+              ? { preset: ['default', { discardComments: { removeAll: true } }] }
+              : false, // disable cssnano when not in production
+    },
+  },
   devtools: { enabled: false }
 })
