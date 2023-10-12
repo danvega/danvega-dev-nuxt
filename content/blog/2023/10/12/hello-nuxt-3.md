94e59b9638ade779fd3f8e25599eb4d5c3691fef
Dan Vega <danvega@gmail.com>
2023-10-12 10:20:39 -0400

    testing out draft mode

---
+++ content/blog/2023/10/12/hello-nuxt-3.md
@@ -0,0 +1,16 @@
+---
+title: 'Title of the page'
+description: 'meta description of the page'
+draft: true
+date: "2024-10-13T17:00:00.000Z"
+cover:
+video:
+---
+
+This is a new blog post about Spring
+
+```java
+public static void main(String[] args) {
+    Sysstem.out.println("Hello, Spring!");    
+}
+```
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -10,12 +10,20 @@ const { data } = await useAsyncData(`content-${path}`, () => {
 })
 
 // add a type for blog post
-// construct image path (/images/year/month/day/)
 // date format
 const datePublished = useDateFormat(data.date, 'MMMM D, YYYY')
 // date updated
 // time to read?
 
+
+const getYouTubeVideoID = (video) => {
+  let id = "";
+  if(video.length) {
+    const parts = video.split("/");
+    id = parts[parts.length - 1];
+  }
+  return id;
+}
 
 const getImagePath = (date,cover) => {
   const createdOn = new Date(date);
@@ -32,12 +40,6 @@ const getImagePath = (date,cover) => {
     <div class="xl:relative">
       <div class="mx-auto max-w-7xl">
 
-<!--        <button type="button" aria-label="Go back to articles" class="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0">-->
-<!--          <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400">-->
-<!--            <path d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>-->
-<!--          </svg>-->
-<!--        </button>-->
-
         <article>
           <header className="flex flex-col">
             <time dateTime="September 5, 2022" class="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500">
@@ -50,15 +52,18 @@ const getImagePath = (date,cover) => {
           </header>
 
           <!--- if video, show video -->
-          <!--- https://betterprogramming.pub/4-steps-to-embed-youtube-in-nuxt-js-with-a-markdown-file-9d5fe9a0167a -->
-          <!-- https://github.com/paulirish/lite-youtube-embed -->
-
+          <iframe :src="data.video"
+                  class="w-full aspect-video"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+                  v-if="data.video"></iframe>
           <!-- else show image -->
           <img :src="getImagePath(data.date,data.cover)"
                class="prose dark:prose-invert rounded-2xl mt-8"
-               alt="Spring JDBC Client"/>
+               alt="ALT TEXT"
+               v-else-if="data.cover"/>
 
           <ContentRenderer :value="data" class="prose dark:prose-invert mt-8" />
+
         </article>
 
         <!-- tags -->
