b6881e98e8e4e201554338e411bdfef2165dc313
Dan Vega <danvega@gmail.com>
2023-10-18 16:12:54 -0700

    updating uses content desc

--- pages/uses.vue
+++ pages/uses.vue
@@ -109,58 +109,59 @@ useHead({
             <div class="md:col-span-3">
               <ul role="list" class="space-y-16">
                 <li class="group relative flex flex-col items-start">
-                  <a href="hhttps://amzn.to/3F5XnG8">
+                  <a href="https://amzn.to/3F5XnG8">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Sony A6100</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">The Sony A6100 has become my go-to gear for creating stunning YouTube videos. Its ability to shoot in sharp 4K resolution ensures that every detail is captured crisply, making my content visually engaging. Moreover, the ease of use and reliable autofocus system lets me focus more on the content creation rather than fiddling with camera settings, making my filming process smooth and efficient.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://www.danvega.dev/uses/Sony%20A6100">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Sigma 16mm f1.4 lens</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">The Sigma 16mm f1.4 lens has truly elevated my YouTube videos, delivering that sought-after blurry background which adds a professional depth to my shots. The wide aperture of f1.4 not only allows for beautiful bokeh but also performs exceptionally in low light conditions, ensuring my videos are always vibrant and clear. Its sharp and precise focus adds a level of polish to my content, making each video a visual delight for my viewers.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
-                  <a href="https://amzn.to/3dWTkwX">
-                    <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Audio-Technica AT2020 Microphone</h3>
+                  <a href="https://amzn.to/3s31Nv9">
+                    <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Shure MV7 USB Microphone</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                    The Shure MV7 USB Microphone has been a game changer in the audio quality of both my YouTube videos and podcast episodes. Its plug-and-play simplicity, coupled with the superior sound clarity, ensures my voice comes across as natural and professional, enhancing the overall auditory experience for my audience. Additionally, the MV7's sleek design and robust build not only look great on camera but also promise longevity, making it a worthy investment for any content creator.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://amzn.to/46UJYNV">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Elgato Cam Link 4K</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">The Elgato Cam Link 4K has seamlessly integrated into my YouTube video creation process, enabling me to capture high-quality video swiftly and efficiently directly into my computer. Its plug-and-play functionality eliminates any hassle, allowing me to focus solely on producing engaging content. The crisp 4K resolution capture ensures that every detail is impeccably recorded, significantly elevating the visual appeal and professionalism of my videos.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://www.danvega.dev/uses/StreamYard">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Wyze Bluetooth Headphones</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">he Wyze Bluetooth Headphones are a delightful find, offering a perfect balance between quality and affordability, which is crucial for my content creation journey. The sound clarity enhances both my editing process and my music listening experience while working, ensuring an enjoyable and productive atmosphere. Moreover, their inexpensive price tag is a sweet bonus, proving that high-quality audio gear doesn't have to break the bank, making them a valuable addition to my toolkit.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://amzn.to/2X3qPXG">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Elgato Key Lights</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">The Elgato Key Lights have become an indispensable part of my video creation setup, providing the precise lighting I need to produce high-quality videos. Their slim profile is not only aesthetically pleasing but also space-saving, and the convenient attachment to my desk ensures a stable setup while keeping my workspace uncluttered. The easy adjustability of brightness and color temperature directly from my computer or smartphone allows me to effortlessly create the perfect ambiance for each video, enhancing the visual quality of my content significantly.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://www.adobe.com/products/premiere.html">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Adobe Premiere Pro</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">Adobe Premiere Pro has become an extension of my creativity, providing a robust platform to edit all my videos with finesse. The plethora of editing tools, coupled with its seamless integration with other Adobe applications, enables me to bring my vision to life in a professional and visually appealing manner. The intuitive interface and the endless possibilities for customization make every editing session a smooth and enjoyable experience, ensuring each of my videos is polished to perfection.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://www.epidemicsound.com/">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Epidemic Sound</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">Epidemic Sound has been a treasure trove of high-quality soundtracks that add a unique vibe to each of my videos. Its vast library and user-friendly interface make finding the perfect tune a breeze, ensuring my content is always sonically engaging and complementary to the visual narrative.</p>
                 </li>
                 <li class="group relative flex flex-col items-start">
                   <a href="https://www.ecamm.com/">
                     <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Ecamm Live</h3>
                   </a>
-                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">Ecamm Live has revolutionized the way I record screencasts and go live on YouTube, offering a seamless, user-friendly interface coupled with robust functionality. The software provides a smooth recording and streaming experience, ensuring high-quality output that engages my audience whether I'm sharing my screen or interacting live, making every session professional and enjoyable.</p>
                 </li>
               </ul>
             </div>
