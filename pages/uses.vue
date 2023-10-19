7a8f6111f1da3aa03b69a60a6e901ff44b3d712a
Dan Vega <danvega@gmail.com>
2023-10-18 22:34:04 -0700

    adding videotap to uses page

--- pages/uses.vue
+++ pages/uses.vue
@@ -177,6 +177,10 @@ useHead({
                   <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">Notion</h3>
                   <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
                 </li>
+                <li class="group relative flex flex-col items-start">
+                  <h3 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">VideoTap</h3>
+                  <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400"></p>
+                </li>
               </ul>
             </div>
           </div>
