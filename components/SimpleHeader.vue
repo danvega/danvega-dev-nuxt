9321080ce90157135c099b6c26a7fd63eb2d60d0
Dan Vega <danvega@gmail.com>
2023-12-14 14:40:31 -0500

    Adding Alt Text to avatar

--- components/SimpleHeader.vue
+++ components/SimpleHeader.vue
@@ -33,7 +33,7 @@ const navItems: NavItem[] = [
                   <div class="h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
                     <!--- avatar -->
                     <a aria-label="Home" class="pointer-events-auto" href="/">
-                      <img alt="" width="512" height="512"
+                      <img alt="Dan Vega" width="512" height="512"
                            class="rounded-full bg-zinc-100 object-cover dark:bg-zinc-800 h-9 w-9"
                            style="color: transparent;" src="/images/dan_avatar.jpg">
                     </a>
