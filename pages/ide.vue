eb7c4390d73fd8c746e0a2b80508f24201f235ad
Dan Vega <danvega@gmail.com>
2024-04-01 16:17:07 -0400

    Adding a page to refer people to when asked about my IDE setup and theme.

---
+++ pages/ide.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup="">
+const title = 'Dan Vega - IntelliJ IDE Setup';
+useHead({
+  title: title,
+  meta: [
+    { name: 'title', content: title },
+    { name: 'description', content: 'I always get asked what the theme in my IDE is or how I set it up.' }
+  ]
+});
+</script>
+
+
+<template>
+  <Container class="mt-16 sm:mt-32">
+    <header class="max-w-4xl">
+      <h1 class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+         My IDE Setup
+      </h1>
+      <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+        I am often asked about what IDE I use, my current theme, plugins or how something I did works. I am going to use this page to document answers to those types
+      of questions.</p>
+    </header>
+
+    <div class="mt-8 sm:mt-8">
+
+          <p class="mb-8">My current IDE is <a href="https://www.jetbrains.com/idea/">IntelliJ IDEA Ultimate Edition</a>. I made a video below documenting my theme and how I use it. I will try and update this page soon with more IDE tips and tricks</p>
+
+          <you-tube id="g78is10FjF0"/>
+
+    </div>
+
+  </Container>
+</template>
