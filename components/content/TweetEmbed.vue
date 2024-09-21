4527a0d1510c5b17b5474338d358b6bbf8513cc5
Dan Vega <danvega@gmail.com>
2024-09-21 08:20:17 -0400

    Adding TweetEmbed component for markdown

---
+++ components/content/TweetEmbed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tweet-embed">
+    <blockquote class="twitter-tweet" :data-theme="theme">
+      <a :href="tweetUrl">Loading tweet...</a>
+    </blockquote>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  theme: {
+    type: String,
+    default: 'light'
+  }
+})
+
+const tweetUrl = `https://twitter.com/x/status/${props.id}`
+
+onMounted(() => {
+  // Load Twitter widget script
+  const script = document.createElement('script')
+  script.setAttribute('src', 'https://platform.twitter.com/widgets.js')
+  script.setAttribute('async', true)
+  document.head.appendChild(script)
+})
+</script>
