4d74c07b1618ae7788109b19649dbef6d6f7b6e7
danvega <danvega@gmail.com>
2025-09-15 10:59:32 -0400

    Adding custom RSS feed becuase feedme isn't working

--- types/index.ts
+++ types/index.ts
@@ -9,6 +9,40 @@ declare global {
         description: string
     }
 
+    // RSS Feed Types
+    type BlogPost = {
+        title?: string;
+        description?: string;
+        slug?: string;
+        date: string;
+        published: boolean;
+        author?: string;
+        tags?: string[];
+        _path?: string;
+        body?: string;
+    }
+
+    type RSSItem = {
+        title: string;
+        description: string;
+        link: string;
+        guid: string;
+        pubDate: string;
+        author: string;
+        categories: string[];
+    }
+
+    type RSSFeed = {
+        title: string;
+        description: string;
+        link: string;
+        language: string;
+        lastBuildDate: string;
+        managingEditor: string;
+        webMaster: string;
+        items: RSSItem[];
+    }
+
     type Photo = {
         id: number,
         width: number,
