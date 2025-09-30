98e9f5d5372c95ca9b04e69db8c498721d24cfb7
danvega <danvega@gmail.com>
2025-09-30 14:11:11 -0600

    Trying to correct spring io 2025 image that was flipped (exif data)

--- data/photos.ts
+++ data/photos.ts
@@ -150,9 +150,8 @@ export const photos: Photo[] = [
     id: 21,
     width: 1024,
     height: 768,
-    src: '/images/photos/spring_io_2025_01.jpeg',
-    alt: 'Spring/IO 2025: My First Session',
-    rotation: 180
+    src: '/images/photos/spring_io_2025_01_flipped.jpeg',
+    alt: 'Spring/IO 2025: My First Session'
   },
   {
     id: 22,
